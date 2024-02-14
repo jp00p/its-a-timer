5b7824ed0a84d2cdf8ca691c5d3e2978397e9e42
Jpop Wilson <asimovsghost@gmail.com>
2024-02-14 10:25:49 -0800

    type hinting

--- main.gd
+++ main.gd
@@ -9,23 +9,21 @@ var timers = [
     },
 ]
 
-@export var current_timer_idx = 0 : set = set_timer_idx
-
-@onready var timer = %Timer
-@onready var progress = %Progress
-@onready var label = %TextLabel
-@onready var time_remaining = %TimerText
-@onready var audio = %Audio
-@onready var popup = %Window
-@onready var reward = %Reward
-@onready var add_timer = %AddTimer
+var current_timer_idx = 0 : set = set_timer_idx
+@onready var timer:Timer = %Timer
+@onready var progress:ProgressBar = %Progress
+@onready var label:RichTextLabel = %TextLabel
+@onready var time_remaining:RichTextLabel = %TimerText
+@onready var audio:AudioStreamPlayer2D = %Audio
+@onready var popup:Window = %Window
+@onready var reward:TextureRect = %Reward
+@onready var add_timer:Window = %AddTimer
 
 var current_timer:Dictionary
 var styles:StyleBoxFlat
 var rewards:Array = []
-
-var local_path = OS.get_executable_path().get_base_dir()
-var rewards_folder_name = "/rewards/"
+var local_path:String = OS.get_executable_path().get_base_dir()
+var rewards_folder_name:String = "/rewards/"
 
 func _ready():
     randomize()
@@ -33,6 +31,9 @@ func _ready():
     load_rewards()
     set_current_timer()
 
+## Load reward images from the directory
+## Create directory if it doesn't exist
+## TODO: Live reload reward images
 func load_rewards():
     var rewards_path = local_path + rewards_folder_name
     var rewards_source = "res://rewards/"
@@ -44,6 +45,7 @@ func load_rewards():
         var reward_image = Image.load_from_file(reward)
         rewards.push_front(ImageTexture.create_from_image(reward_image))
 
+## Set the active timer (based on current_timer_idx)
 func set_current_timer():
     current_timer = timers[current_timer_idx]
     var timer_total_time = max(1, (current_timer["minutes"] * 60 + current_timer["seconds"]))
@@ -58,30 +60,38 @@ func set_current_timer():
     if rewards.size() > 0:
         reward.texture = rewards.pick_random()
 
-func update_text():
+## Update text in the UI
+## Also updates progress bar
+func update_text() -> void:
     label.text = "%s" % current_timer["name"]
     time_remaining.text = "Time remaining: %02d:%02d" % [int(timer.time_left/60), (int(timer.time_left) % 60)]
     progress.value = timer.time_left
 
-func _process(_delta):
+## Every frame
+func _process(_delta) -> void:
     update_text()
     %RemoveTimer.disabled = timers.size() <= 1
 
-func _input(_event):
+## Listen for user input
+## F1 will perform a sound test
+func _input(_event) -> void:
     if Input.is_action_just_pressed("soundtest"):
         audio.play()
 
-func _on_play_pressed():
+## When play/pause button is pressed
+func _on_play_pressed() -> void:
     if timer.is_stopped():
         timer.start()
     else:
         timer.paused = !timer.paused
 
-func _on_stop_pressed():
+## When stop is pressed
+func _on_stop_pressed() -> void:
     audio.stop()
     timer.stop()
 
-func _on_next_pressed():
+## When next is pressed
+func _on_next_pressed() -> void:
     popup.hide()
     audio.stop()
     timer.stop()
@@ -90,10 +100,11 @@ func _on_next_pressed():
 
 ## Set timer index
 ## Wrap around size of timers list
-func set_timer_idx(val):
+func set_timer_idx(val:int) -> void:
     current_timer_idx = wrapi(val, 0, len(timers))
 
-func _on_timer_timeout():
+## When timer finishes
+func _on_timer_timeout() -> void:
     self.current_timer_idx += 1
     audio.play()
     set_current_timer()
@@ -101,11 +112,12 @@ func _on_timer_timeout():
     if rewards.size() > 0:
         popup.show()
 
-func _on_window_close_requested():
+## Rewards window close button
+func _on_window_close_requested() -> void:
     popup.hide()
 
 ## Finalize creating new timer
-func _on_add_timer_pressed():
+func _on_add_timer_pressed() -> void:
     var timer_name = "Unnamed Timer"
     if %TimerName.text != "":
         timer_name = %TimerName.text
@@ -125,7 +137,7 @@ func _on_add_timer_pressed():
 
 ## List directory contents
 ## Returns array of filenames
-func dir_contents(path) -> Array:
+func dir_contents(path:String) -> Array:
     var dir = DirAccess.open(path)
     var files = []
     if dir:
@@ -140,17 +152,17 @@ func dir_contents(path) -> Array:
     return files
 
 ## Close "add timer" popup
-func _on_add_timer_close_requested():
+func _on_add_timer_close_requested() -> void:
     add_timer.hide()
 
 ## Click "add timer" button
-func _on_add_new_timer_pressed():
+func _on_add_new_timer_pressed() -> void:
     %ChooseColor.color = Color(randi(), 1.0)
     add_timer.show()
 
 ## Click "remove timer" button
 ## Only works if there's more than 1 timer
-func _on_remove_timer_pressed():
+func _on_remove_timer_pressed() -> void:
     if timers.size() <= 1:
         return
     audio.stop()
